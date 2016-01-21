47181d9c22a2c3cc8c40b686da7301712eba704b
hirdbluebird <bluebirdyourdestiny@gmail.com>
2016-01-21 21:30:01 +0200

    add trigger on shift + enter

--- js/all.js
+++ js/all.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
 		var textareaValue = $("#postForm textarea").val();
 		$('#postWrapper').append('<section class="single-post"><p class="post-inform"><span class="name">Имануил</span><span class="post-data">13 октября 2011</span></p><p class="post">'+ textareaValue + '</p></section>');
 	})
+	$("textarea").keypress(function(event) {	
+		if (event.keyCode == 13 && event.shiftKey) {
+	    	var textareaValue = $("#postForm textarea").val();
+			$('#postWrapper').append('<section class="single-post"><p class="post-inform"><span class="name">Имануил</span><span class="post-data">13 октября 2011</span></p><p class="post">'+ textareaValue + '</p></section>');
+		}
+	});
 })
